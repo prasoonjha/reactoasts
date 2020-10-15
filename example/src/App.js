46d0185c57511206f1659756e176c1c2bf3bfb04
Prasoon Jha <prasoonjha69@gmail.com>
2020-10-15 21:20:58 +0530

    delete

--- example/src/App.js
+++ example/src/App.js
@@ -1,60 +1,174 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
-import Toast from 'reactoasts'
+import './App.css';
+import Toast from 'reactoasts';
+
+import checkIcon from './assets/check.svg';
+import errorIcon from './assets/error.svg';
+import infoIcon from './assets/info.svg';
+import warningIcon from './assets/warning.svg';
+
+import Button from './button/Button';
+
+const BUTTON_PROPS = [
+  {
+    id: 1,
+    type: 'success',
+    className: 'success',
+    label: 'Success'
+  },
+  {
+    id: 2,
+    type: 'danger',
+    className: 'danger',
+    label: 'Danger'
+  },
+  {
+    id: 3,
+    type: 'info',
+    className: 'info',
+    label: 'Info'
+  },
+  {
+    id: 4,
+    type: 'warning',
+    className: 'warning',
+    label: 'Warning'
+  },
+];
 
 const App = () => {
-  const [type, setType] = useState('success');
-  return <div style={{"padding":"2em 4em", color:"#fff"}}>
-    <select value={type} onChange={(e)=>{setType(e.target.value)}} >
-      <option value='success'>Success</option>
-      <option value='error'>Error</option>
-      <option value='warning'>Warning</option>
-      <option value='info'>Info</option>
-    </select>
-    <button onClick={()=>Toast({
-            position: 'topLeft',
-            type: type,
-            text: 'created successfully'
-        })}>Toast {type} top left</button>
-        <button onClick={()=>Toast({
-            position: 'topRight',
-            type: type,
-            text: 'created successfully'
-        })}>Toast {type} top right</button>
-        <button onClick={()=>Toast({
-            position: 'bottomRight',
-            type: type,
-            text: 'created successfully'
-        })}>Toast {type} bottom right</button>
-        <button onClick={()=>Toast({
-            position: 'bottomLeft',
-            type: type,
-            text: 'created successfully'
-        })}>Toast {type} bottom leftt</button>
-        <div>
-          <p>yarn add neo-toast</p>
-          <code style={{"backgroundColor": "rgba(3, 169, 244, 0.3)",
-          "display": "block", "padding": "2em", "borderRadius": "4px"}}>
-            <br />
-            <br />
-            import Toast from 'neo-toast'
-            <br />
-            <br />
-          <b>
-<pre>
-{`<button
-  onClick={Toast({
-    position: 'topLeft',
-    type: ${type},
-    text: 'created successfully'
-  })>Toast ${type}
-</button>`}
-</pre>
-          </b>
+  const [list, setList] = useState([]);
+  const [position, setPosition] = useState('Select Position');
+  let [checkValue, setCheckValue] = useState(false);
+  const [autoDeleteTime, setAutoDeleteTime] = useState(0);
+  let toastProperties = null;
 
-          </code>
+  const selectPosition = (e) => {
+    setPosition(e.target.value);
+    setList([]);
+  }
+
+  const showToast = type => {
+    const id = Math.floor((Math.random() * 101) + 1);
+
+    switch(type) {
+      case 'success':
+        toastProperties = {
+          id,
+          title: 'Success',
+          description: 'This is a success toast component',
+          backgroundColor: '#5cb85c',
+          icon: checkIcon
+        }
+        break;
+      case 'danger':
+        toastProperties = {
+          id,
+          title: 'Danger',
+          description: 'This is a error toast component',
+          backgroundColor: '#d9534f',
+          icon: errorIcon
+        }
+        break;
+      case 'info':
+        toastProperties = {
+          id,
+          title: 'Info',
+          description: 'This is an info toast component',
+          backgroundColor: '#5bc0de',
+          icon: infoIcon
+        }
+        break;
+      case 'warning':
+        toastProperties = {
+          id,
+          title: 'Warning',
+          description: 'This is a warning toast component',
+          backgroundColor: '#f0ad4e',
+          icon: warningIcon
+        }
+        break;
+
+        default:
+          setList([]);
+    }
+
+    setList([...list, toastProperties]);
+  }
+  
+   const onCheckBoxChange = () => {
+    checkValue = !checkValue;
+    setCheckValue(checkValue);
+    setList([]);
+  }
+
+  const onInputChange = (e) => {
+    const time = parseInt(e.target.value, 10);
+    setAutoDeleteTime(time);
+  }
+
+  return (
+    <div className="app">
+      <div className="app-header">
+        <p>React Toast Component</p>
+        <div className="toast-buttons">
+          {
+            BUTTON_PROPS.map(e => 
+              <Button 
+                key={e.id}
+                className={`${position === 'Select Position' ? `${e.className} btn-disable` : `${e.className}`}`}
+                label={e.label}
+                handleClick={() => showToast(e.type)}
+              />
+            )
+          }
         </div>
-  </div>
+
+        <div className="select">
+          <input 
+            id="auto"
+            type="checkbox"
+            name="checkbox"
+            value={checkValue}
+            onChange={onCheckBoxChange}
+          />
+          <label htmlFor="auto">Auto Dismiss</label>
+        </div>
+        <div className="select">
+          <input 
+            className={`${!checkValue ? 'disabled' : ''}`}
+            type="text"
+            name="checkbox"
+            placeholder="Dismiss time Ex: 3000"
+            autoComplete="false"
+            onChange={onInputChange}
+          />
+        </div>
+        <div className="select">
+          <select
+            name="position"
+            value={position}
+            onChange={selectPosition}
+            className="position-select"
+          >
+            <option>Select Position</option>
+            <option value="top-right">Top Right</option>
+            <option value="top-left">Top Left</option>
+            <option value="bottom-left">Bottom Left</option>
+            <option value="bottom-right">Bottom Right</option>
+          </select>
+        </div>
+      </div>
+
+      <Toast 
+        toastList={list}
+        position={position}
+        autoDelete={checkValue}
+        autoDeleteTime={autoDeleteTime}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
